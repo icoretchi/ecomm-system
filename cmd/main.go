67ba071132a458bad164ba26f31f7155aa269cee
Iulian Coretchi <icoretchi@Iulians-MacBook-Pro.local>
2022-07-19 10:04:05 +0300

    added tests and coverage

--- cmd/main.go
+++ cmd/main.go
@@ -1,9 +1,10 @@
 package main
 
 import (
-	"encoding/json"
 	"log"
 	"net/http"
+
+	"github.com/icoretchi/ecomm-system/handlers/rest"
 )
 
 func main() {
@@ -12,18 +13,7 @@ func main() {
 
 	mux := http.NewServeMux()
 
-	mux.HandleFunc("/hello",
-		func(w http.ResponseWriter, r *http.Request) {
-			enc := json.NewEncoder(w)
-			w.Header().Set("Content-Type", "application/json; charset=utf-8")
-			resp := Resp{
-				Language:    "English",
-				Translation: "Hello",
-			}
-			if err := enc.Encode(resp); err != nil {
-				panic("unable to encode response")
-			}
-		})
+	mux.HandleFunc("/translate/hello", rest.TranslateHandler)
 
 	log.Printf("listening on %s\n", addr)
 
